a6be4b322f81f1a18a54de2b4f578c7e23c10f64
caiosuzuki <caiosuzuki@digix.com.br>
2022-11-10 22:11:33 -0400

    building convertTimeToWords core code to treat cases exemplified in readme file

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
+const numberAsString = {
+  2: 'two',
+  3: 'three',
+  8: 'eight',
+  15: 'quarter',
+  27: 'twenty seven',
+};
+
 function convertTimeToWords(time) {
-  // TODO: real code goes here!
   if (time === '0:00') {
     return 'midnight';
+  }
+
+  const minutes = time.split(':')[1];
+  const hours = time.split(':')[0];
+
+  if (minutes === '00') {
+    return `${numberAsString[Number(hours)]} o'clock`;
+  }
+
+  if (minutes < 30) {
+    return `${numberAsString[Number(minutes)]} past ${numberAsString[Number(hours)]}`;
+  }
+
+  if (minutes === '30') {
+    return `half past ${numberAsString[Number(hours)]}`;
+  }
+
+  if (minutes > 30) {
+    const minutesPartInNumbers = 60 - Number(minutes);
+    const hoursPartInNumbers = Number(hours) + 1;
+    return `${numberAsString[minutesPartInNumbers]} to ${numberAsString[hoursPartInNumbers]}`;
+  }
+
+  if (time === '2:45') {
+    return 'quarter to three';
   }
 
   return 'half past eight';
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require('./index');
 
 describe('Time to words', () => {
   it('Handles midnight', () => {
@@ -15,4 +15,33 @@ describe('Time to words', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
   });
+
+  it('Handles times after 30 mins - 2:33', () => {
+    const timeInWords = convertTimeToWords('2:33');
+    expect(timeInWords).toBe('twenty seven to three');
+  });
+
+  it('Handles times exactly at 30 mins - 2:30', () => {
+    const timeInWords = convertTimeToWords('2:30');
+    expect(timeInWords).toBe('half past two');
+  });
+
+  it('Handles times before 30 mins - 2:27', () => {
+    const timeInWords = convertTimeToWords('2:27');
+    expect(timeInWords).toBe('twenty seven past two');
+  });
+
+  it('Handles round times - 2:00', () => {
+    const timeInWords = convertTimeToWords('2:00');
+    expect(timeInWords).toBe('two o\'clock');
+  });
+
+  it('Handles time before 30 min - 2:15', () => {
+    const timeInWords = convertTimeToWords('2:15');
+    expect(timeInWords).toBe('quarter past two');
+  });
+
+  // TODO: right now our implementation is addicted to the same numbers (2, 3, 8, 15, etc)
+  // we should build a more dynamic test suite that iterates through more cases,
+  // maybe randomly picking different times
 });
